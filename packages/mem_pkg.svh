89c86af8089deb8848c8a9d642b09860dc0178f8
Aniruddh Mishra <aniruddhm17@gmail.com>
2025-12-02 02:22:49 -0600

    packages only imported once

--- packages/mem_pkg.svh
+++ packages/mem_pkg.svh
@@ -1,7 +1,6 @@
 package mem_pkg;
     parameter int unsigned BLOCK_BYTES = 64; // cell size bytes
     parameter int unsigned NUM_BLOCKS = 4096; // number of memory cells
-    parameter int unsigned DATA_WIDTH = 8;
     localparam int unsigned ADDR_W = $clog2(NUM_BLOCKS); // ptr size
     localparam int unsigned FOOTER_BYTES = 2; // last 8 B of cell used to point to next cell
     localparam int unsigned PAYLOAD_BYTES= BLOCK_BYTES-FOOTER_BYTES; // 56 B
