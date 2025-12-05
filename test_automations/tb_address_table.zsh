dc6657630de41707ec4f1429d3c3f8fa56fe752d
Aniruddh Mishra <aniruddhm17@gmail.com>
2025-12-04 23:50:41 -0600

    test script

---
+++ test_automations/tb_address_table.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+verilator -sv --binary --trace packages/switch_pkg.sv packages/address_table_pkg.sv tests/tb_address_table.sv src/address_table.sv --top-module tb_address_table -o sim_address_table
+
+./obj_dir/sim_address_table
+
+gtkwave tb_address_table.vcd
