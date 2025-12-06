199c6787817eae2c7309f56591bfc2cd2b85d79b
Aniruddh Mishra <aniruddhm17@gmail.com>
2025-12-06 15:19:12 -0600

    reduced number of entries

--- packages/address_table_pkg.sv
+++ packages/address_table_pkg.sv
@@ -1,10 +1,9 @@
 package address_table_pkg;
-    parameter int NUM_ENTRIES = 16; // Number entries in the addres learning table (~4 per port)
+    parameter int NUM_ENTRIES = 4; // Number entries in the addres learning table (~4 per port)
     parameter int MAX_HIT = 16; // Maximum number of hits in the hit table before saturation
 
     // Priority encoder function
     function automatic [$clog2(NUM_ENTRIES)-1:0] free_index(logic table_usage [NUM_ENTRIES-1:0], logic [$clog2(MAX_HIT)-1:0] table_hits [NUM_ENTRIES-1:0]);
-        return 0;
         logic [$clog2(NUM_ENTRIES)-1:0] return_index;
         return_index = min_used(table_hits);
         for (int i=0; i<NUM_ENTRIES; i=i+1) begin
