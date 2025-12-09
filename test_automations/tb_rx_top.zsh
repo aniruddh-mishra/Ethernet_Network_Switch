0314bac9674bd76b87d309a1ce51ce81c73bb472
Aniruddh Mishra <aniruddhm17@gmail.com>
2025-12-09 02:02:53 -0600

    test file

---
+++ test_automations/tb_rx_top.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+verilator -sv --binary --trace packages/switch_pkg.sv packages/rx_tx_pkg.sv packages/mem_pkg.sv tests/tb_rx_top.sv src/rx_top.sv src/rx_mac_control.sv src/memory_read_ctrl.sv src/mac_controllers.sv src/memory_write_ctrl.sv src/sram.sv src/fl.sv src/async_fifo.sv src/arbiter.sv --top-module tb_rx_top -o sim_rx_top
+
+./obj_dir/sim_rx_top
+
+gtkwave tb_rx_top.vcd
