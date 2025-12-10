5b694d94a7a6b0ce3ff8f382ce143d0bc8c83ad8
Aniruddh Mishra <aniruddhm17@gmail.com>
2025-12-10 01:19:25 -0600

    temp fix fl

--- src/fl.sv
+++ src/fl.sv
@@ -22,6 +22,17 @@ module fl (
     assign empty = sp == 0;
     assign full = {25'b0, sp} == NUM_BLOCKS;
 
+    logic exists_in_stack;
+    // check if free_block_idx_i is already in stack
+    always_comb begin
+        exists_in_stack = 0;
+        for (int i = 1; i <= sp; i++) begin
+            if (stack[i] == free_block_idx_i) begin
+                exists_in_stack = 1;
+            end
+        end
+    end
+
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             sp <= NUM_BLOCKS[ADDR_W:0];
@@ -32,7 +43,7 @@ module fl (
         end
         else begin
             alloc_gnt_o <= 0;
-            if (alloc_req_i && free_req_i) begin
+            if (alloc_req_i && free_req_i && exists_in_stack) begin
                 // sp not updated
                 alloc_block_idx_o <= free_block_idx_i;
                 alloc_gnt_o <= 1;
@@ -43,7 +54,7 @@ module fl (
                 sp <= sp - 1;
                 alloc_gnt_o <= 1;
             end
-            else if (free_req_i && !full) begin
+            else if (free_req_i && !full && exists_in_stack) begin
                 // push to stack
                 stack[sp + 1] <= free_block_idx_i;
                 sp <= sp + 1;
